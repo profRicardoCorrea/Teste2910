a9f0380b60c02e55db9990f19d9ee77445bc8e4d
Ricardo Corres <111370109+profRicardoCorrea@users.noreply.github.com>
2025-10-29 19:47:08 -0300

    criacao de arquivos

---
+++ src/a.java
@@ -0,0 +1,11 @@
+public class a {
++    public static void main(String[] args) {
++        PessoaL p1 = new PessoaL();
++        System.out.println("NOME:" + p1.getNome());
++        System.out.println("CPF:" + p1.getCpf());
++        System.out.println("Email:" + p1.getEmail());
++        System.out.println("Endereco:" + p1.getEndereco());
++        System.out.println("Idade:" + p1.getIdade());
++        System.out.println("Telefone:" + p1.getTelefone());
++    }
+}
